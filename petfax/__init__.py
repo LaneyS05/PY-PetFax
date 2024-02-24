d537b2a19f7b368a11b3feab6b5df101318a789b
LaneyS05 <laney.staggs05@gmail.com>
2024-02-24 13:32:23 -0500

    adding update and delete

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .pets import bp as pets_bp
 from .facts import bp as facts_bp
 
-# factory 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
 
-    # register pet blueprint 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+
+
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+
+
+    # Register blueprints
     app.register_blueprint(pets_bp)
     app.register_blueprint(facts_bp)
 
@@ -14,6 +27,6 @@ def create_app():
     def index():
         return render_template('home.html') 
 
-    # return the app 
     return app
+
 
--- petfax/facts.py
+++ petfax/facts.py
@@ -1,18 +1,46 @@
-from flask import (Blueprint, render_template, request, redirect) 
+from flask import Blueprint, render_template, request, redirect
+from . import models
 
-# Create a new blueprint for facts
 bp = Blueprint('fax', __name__, url_prefix="/new")
 
 @bp.route('/', methods=['GET'])
 def new_fact_form():
     return render_template('fax.html')  
-
+       
 @bp.route('/facts', methods=['GET', 'POST']) 
-def fact(): 
+def fact():
     if request.method == 'POST':
-       print(request.form)
-       return redirect('/new') 
+        submitter = request.form['name']
+        fact_text = request.form['fact']
 
+        new_fact = models.Fact(submitter=submitter, fact=fact_text)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+        return redirect('/new/facts')
 
+    results = models.Fact.query.all()
+    return render_template('facts.html', facts=results)
 
+@bp.route('/update/<int:fact_id>', methods=['GET', 'POST']) 
+def update(fact_id):
+    fact = models.Fact.query.get_or_404(fact_id)
+
+    if request.method == 'POST':
+        submitter = request.form['name']
+        fact_text = request.form['fact']
+
+        fact.submitter = submitter
+        fact.fact = fact_text
+
+        models.db.session.commit()
+        return redirect('/new/facts')
+
+    return render_template('update.html', fact=fact)
+
+@bp.route('/delete/<int:fact_id>', methods=['POST'])
+def delete(fact_id): 
+    fact = models.Fact.query.get_or_404(fact_id)
+    models.db.session.delete(fact)
+    models.db.session.commit()
+    return redirect('/new/facts')
 
